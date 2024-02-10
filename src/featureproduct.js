8dbfd6153f16d427ae2694ad1fad80579ebc3150
Deepanjali <deepanjali20@navgurukul.org>
2024-02-11 01:17:02 +0530

    feature complete

---
+++ src/featureproduct.js
@@ -0,0 +1,51 @@
+import './App.css';
+const Featureproduct = () => {
+    return (
+        <>
+            <div className='mainfeature' >
+                <div className='festuretext'>
+                    <h2>Featured Products</h2>
+                </div>
+                <div>
+                    <div className='feature'> 
+                                           <div className='featureinfo' >
+                        <div className='featurimage'> <img src="image 3.png" alt="Image 1" />
+                        </div>
+                        <div><ul class="plan-features">
+                            <li>.MEALBOXES</li>
+                            <li>Rakhi inspired</li>
+                            <li>Description of the product</li><br />
+                            <li>⭐⭐⭐⭐⭐</li>
+                            <li>₹900.00 ₹850.00</li>
+                        </ul> </div>
+                        </div>
+                        <div className='featureinfo' >
+                        <div className='featurimage'> <img src="image 3.png" alt="Image 1" />
+                        </div>
+                        <div><ul class="plan-features">
+                            <li>.MEALBOXES</li>
+                            <li>Rakhi inspired</li>
+                            <li>Description of the product</li><br />
+                            <li>⭐⭐⭐⭐⭐</li>
+                            <li>₹900.00 ₹850.00</li>
+                        </ul> </div>
+                        </div>
+                        <div className='featureinfo' >
+                        <div className='featurimage'> <img src="image 3.png" alt="Image 1" />
+                        </div>
+                        <div><ul class="plan-features">
+                            <li>.MEALBOXES</li>
+                            <li>Rakhi inspired</li>
+                            <li>Description of the product</li><br />
+                            <li>⭐⭐⭐⭐⭐</li>
+                            <li>₹900.00 ₹850.00</li>
+                        </ul> </div>
+                        </div>
+                        </div>
+
+                </div>
+            </div>
+        </>
+    )
+};
+export default Featureproduct;
